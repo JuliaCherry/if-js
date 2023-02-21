16234ccafb667849b0035c0c0a900cb48ce4d6f9
Yuliya Alexandrova <julicherry69@gmail.com>
2023-02-21 18:07:02 +0300

    add lesson -8

--- main.js
+++ main.js
@@ -463,3 +463,75 @@ const deepEqual = (object1, object2) => {
 
 console.log(deepEqual(obj1, obj2));
 console.log(deepEqual(obj1, obj3));
+
+//----8----//
+
+const studentsData = [
+  {
+    firstName: 'Василий',
+    lastName: 'Петров',
+    admissionYear: 2019,
+    courseName: 'Java',
+  },
+  {
+    firstName: 'Иван',
+    lastName: 'Иванов',
+    admissionYear: 2018,
+    courseName: 'JavaScript',
+  },
+  {
+    firstName: 'Александр',
+    lastName: 'Федоров',
+    admissionYear: 2017,
+    courseName: 'Python',
+  },
+  {
+    firstName: 'Николай',
+    lastName: 'Петров',
+    admissionYear: 2019,
+    courseName: 'Android',
+  },
+];
+
+class User {
+  constructor({ firstName, lastName }) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+class Student extends User {
+  constructor({ admissionYear, courseName, ...props }) {
+    super(props);
+    this.admissionYear = admissionYear;
+    this.courseName = courseName;
+  }
+  get course() {
+    return new Date().getFullYear() - this.admissionYear;
+  }
+}
+
+class Students {
+  constructor(studentsData) {
+    this.studentsData = studentsData;
+  }
+
+  getInfo() {
+    return this.studentsData
+      .sort((one, two) => new Student(one).course - new Student(two).course)
+      .map(
+        (value) =>
+          new User(value).fullName +
+          ' - ' +
+          new Student(value).courseName +
+          ', ' +
+          new Student(value).course +
+          ' курс',
+      );
+  }
+}
+const students = new Students(studentsData);
+console.log(students.getInfo());
